b311abb98d3b05531b0b89f0a6d69e4e655e96d2
oligiochi <oligiovi2@gmail.com>
2025-01-15 22:36:56 +0000

    sto faccendo test per capire come funziona

---
+++ MazeGenApp/backtracking/src/main/kotlin/org/mazegenerator/app/App.kt
@@ -0,0 +1,12 @@
+package org.mazegenerator.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+//Da implementare sto capendo
